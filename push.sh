deb89a72525fbace01a836bc411d890ebf79e6af
邹润阳 <zry656565@gmail.com>
2014-11-11 19:05:04 +0800

    push脚本现在拥有更为正确的行为

--- push.sh
+++ push.sh
@@ -3,9 +3,9 @@
 dir_name="SJTU-Bus"
 svn_repo_url="https://svn.sinaapp.com/sjtubus"
 svn_repo_name="sjtubus"
-svn_dir_name="sjtubus/2"
-sae_version=1
-init=true
+svn_dir_name="sjtubus/$2"
+sae_version=$2
+init=$1
 
 if [ $init == false ]
 then
@@ -15,18 +15,24 @@ then
 	cd ..
 fi
 
-echo "copy git repo to SAE-svn"
+echo "remove all previous files..."
+cd ".svn/$svn_dir_name"
+mv config.yaml ../config.yaml
+rm -rf *
+mv ../config.yaml config.yaml
+svn st | awk '{print $2}' | xargs svn delete
+svn commit -m "clean"
+cd ../../..
+
+echo "copy git repo to SAE-svn..."
 mv -f .svn ../.svn
-#protect config.yaml
-#cp -r "../.svn/$svn_dir_name/config.yaml" ../.svn
-#rm -rf "../.svn/$svn_dir_name/"
-#end
 cp -rf * "../.svn/$svn_dir_name"
 mv -f ../.svn .svn
 echo "copy Done."
 cd ".svn/$svn_dir_name"
-echo "svn add all"
+echo "svn add all..."
 #add all files
-svn st | awk '{if ( $1 == "?") { print $2}}' | xargs svn add
-echo "svn commit"
+svn st | awk '{if ( $1 == "?") { print $2 }}' | xargs svn add
+echo "svn commit..."
 svn commit -m "modify"
+echo "If there is no error below, the pushing job has been done."
